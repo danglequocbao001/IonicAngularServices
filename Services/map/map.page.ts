04c1dc8d0a8fccd65cb52539b04fb7bfa8984869
Dang Le Quoc Bao <56766274+danglequocbao001@users.noreply.github.com>
2021-02-26 23:50:40 +0700

    init map (#2)
    * hot fix
    
    * update map
    
    * fix litle

--- Services/map/map.page.ts
+++ Services/map/map.page.ts
@@ -2,140 +2,151 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { GeolocationService } from 'src/app/@app-core/utils';
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.page.html',
-  styleUrls: ['./map.page.scss'],
+    selector: 'app-map',
+    templateUrl: './map.page.html',
+    styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  map: google.maps.Map;
+    map: google.maps.Map;
 
-  center: google.maps.LatLngLiteral = this.GeolocationService.centerService;
+    center: google.maps.LatLngLiteral = this.GeolocationService.centerService;
 
-  infoWindows: any = [];
+    infoWindows: any = [];
 
-  markers: any = []
+    markers = [
+        {
+            location: { lat: 10.2346234, lng: 106.2363665 },
+            name: 'some name',
+            thumb_image: [{ url: 'url of image' }],
+            address: 'an address',
+        },
+        {
+            location: { lat: 10.2346234, lng: 106.2363665 },
+            name: 'some name',
+            thumb_image: [{ url: 'url of image' }],
+            address: 'an address',
+        },
+        {
+            location: { lat: 10.2346234, lng: 106.2363665 },
+            name: 'some name',
+            thumb_image: [{ url: 'url of image' }],
+            address: 'an address',
+        }
+    ]
 
-  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
-  constructor(
-    public platform: Platform,
-    private GeolocationService: GeolocationService,
-    private geolocationService: GeolocationService,
-  ) { }
+    @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+    constructor(
+        public platform: Platform,
+        private GeolocationService: GeolocationService,
+        private geolocationService: GeolocationService,
+    ) { }
 
-  ngOnInit() {
-    this.GeolocationService.getCurrentLocation();
-    this.addDataMarkerToMap();
-  }
-
-  ionViewWillEnter() {
-    this.center = this.GeolocationService.centerService;
-    this.initMap();
-  }
-
-  ngAfterViewInit() {
-  }
-
-  initMap(): void {
-    this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-      center: this.center,
-      zoom: 15,
-      disableDefaultUI: true,
-    });
-  }
-
-  getCurrentLocation() {
-    this.GeolocationService.getCurrentLocation();
-    this.center = this.GeolocationService.centerService;
-    this.initMap();
-    this.addCurrenMarker();
-    this.addDataMarkerToMap();
-  }
-
-  addCurrenMarker() {
-    let currentMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(this.center.lat, this.center.lng),
-      label: 'Vị trí của bạn, kéo thả để thay đổi',
-      icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-      draggable: true,
-    });
-    currentMarker.setMap(this.map);
-    this.getCurrentMarkerLatLng(currentMarker, this.center.lat, this.center.lng);
-  }
-
-  getCurrentMarkerLatLng(currentMarker, lat, lng) {
-    google.maps.event.addListener(currentMarker, 'dragend', function (event) {
-      lat = event.latLng.lat();
-      lng = event.latLng.lng();
-    });
-  }
-
-  addDataMarkerToMap() {
-
-  }
-
-  data = [
-      {
-          
-      }
-  ]
-
-  addMarkersToMap(markers) {
-    for (let marker of markers) {
-      let distance = this.geolocationService.distanceFromUserToPoint(this.center.lat, this.center.lng, marker.location.lat, marker.location.long);
-      let position = new google.maps.LatLng(marker.location.lat, marker.location.long);
-      let mapMarker = new google.maps.Marker({
-        position: position,
-        label: marker.name,
-        icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
-      });
-      let mapMarkerInfo = {
-        name: marker.name,
-        url: marker.thumb_image.url,
-        priest_name: marker.priest_name,
-        address: marker.address,
-        distance: distance,
-        lat: marker.location.lat,
-        lng: marker.location.long,
-      }
-
-      mapMarker.setMap(this.map);
-      this.addInfoWindowToMarker(mapMarker, mapMarkerInfo);
+    ngOnInit() {
+        this.GeolocationService.getCurrentLocation();
+        this.addDataMarkerToMap();
     }
-  }
 
-  async addInfoWindowToMarker(marker, mapMarkerInfo) {
-    let infoWindowContent =
-      '<div *ngIf=" markers.length != null ">' +
-        '<h3 style=" display: block; text-align: center; ">' + mapMarkerInfo.name + '</h3>' +
-        '<img style=" height: 100px; width: 100%; display: block; border-radius: 12px; " src=' + mapMarkerInfo.url + '>' +
-        '<h5 style=" display: block; text-align: center; ">' + mapMarkerInfo.priest_name + '</h5>' +
-        '<h5>' + mapMarkerInfo.address + '</h5>' +
-        '<p>Khoảng cách ước tính: ' + mapMarkerInfo.distance + ' km</p>' +
-        '<p>Latitude: ' + mapMarkerInfo.lat + '</p>' +
-        '<p>Longitude: ' + mapMarkerInfo.lng + '</p>' +
-        '<ion-button id="navigate" style=" --background: #F6C33E; --border-radius: 10px; display: block; ">' + 'Chỉ đường tới đây' + '</ion-button>'
-      '</div>';
-    let infoWindow = new google.maps.InfoWindow({
-      content: infoWindowContent,
-    });
+    ionViewWillEnter() {
+        this.center = this.GeolocationService.centerService;
+        this.initMap();
+    }
 
-    marker.addListener('click', () => {
-      this.closeAllInfoWindows();
-      infoWindow.open(this.map, marker);
+    ngAfterViewInit() {
+    }
 
-      google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
-        document.getElementById('navigate').addEventListener('click', () => {
-          window.open('https://www.google.com/maps/dir/?api=1&destination=' + mapMarkerInfo.lat + ',' + mapMarkerInfo.lng);
+    initMap(): void {
+        this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+            center: this.center,
+            zoom: 15,
+            disableDefaultUI: true,
         });
-      });
-
-    });
-    this.infoWindows.push(infoWindow);
-  }
-
-  closeAllInfoWindows() {
-    for (let window of this.infoWindows) {
-      window.close();
     }
-  }
+
+    getCurrentLocation() {
+        this.GeolocationService.getCurrentLocation();
+        this.center = this.GeolocationService.centerService;
+        this.initMap();
+        this.addCurrenMarker();
+        this.addDataMarkerToMap();
+    }
+
+    addCurrenMarker() {
+        let currentMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(this.center.lat, this.center.lng),
+            label: 'Change your location',
+            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+            draggable: true,
+        });
+        currentMarker.setMap(this.map);
+        this.getCurrentMarkerLatLng(currentMarker, this.center.lat, this.center.lng);
+    }
+
+    getCurrentMarkerLatLng(currentMarker, lat, lng) {
+        google.maps.event.addListener(currentMarker, 'dragend', function (event) {
+            lat = event.latLng.lat();
+            lng = event.latLng.lng();
+        });
+    }
+
+    addDataMarkerToMap() {
+
+    }
+
+    addMarkersToMap(markers) {
+        for (let marker of markers) {
+            let distance = this.geolocationService.distanceFromUserToPoint(this.center.lat, this.center.lng, marker.location.lat, marker.location.long);
+            let position = new google.maps.LatLng(marker.location.lat, marker.location.long);
+            let mapMarker = new google.maps.Marker({
+                position: position,
+                label: marker.name,
+                icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+            });
+            let mapMarkerInfo = {
+                name: marker.name,
+                url: marker.thumb_image.url,
+                address: marker.address,
+                distance: distance,
+                lat: marker.location.lat,
+                lng: marker.location.long,
+            }
+
+            mapMarker.setMap(this.map);
+            this.addInfoWindowToMarker(mapMarker, mapMarkerInfo);
+        }
+    }
+
+    async addInfoWindowToMarker(marker, mapMarkerInfo) {
+        let infoWindowContent =
+            '<div *ngIf=" markers.length != null ">' +
+            '<h3 style=" display: block; text-align: center; ">' + mapMarkerInfo.name + '</h3>' +
+            '<img style=" height: 100px; width: 100%; display: block; border-radius: 12px; " src=' + mapMarkerInfo.url + '>' +
+            '<h5>' + mapMarkerInfo.address + '</h5>' +
+            '<p>Distance: ' + mapMarkerInfo.distance + ' km</p>' +
+            '<p>Latitude: ' + mapMarkerInfo.lat + '</p>' +
+            '<p>Longitude: ' + mapMarkerInfo.lng + '</p>' +
+            '<ion-button id="navigate" style=" --background: #F6C33E; --border-radius: 10px; display: block; ">' + 'Navigate here' + '</ion-button>'
+        '</div>';
+        let infoWindow = new google.maps.InfoWindow({
+            content: infoWindowContent,
+        });
+
+        marker.addListener('click', () => {
+            this.closeAllInfoWindows();
+            infoWindow.open(this.map, marker);
+
+            google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+                document.getElementById('navigate').addEventListener('click', () => {
+                    window.open('https://www.google.com/maps/dir/?api=1&destination=' + mapMarkerInfo.lat + ',' + mapMarkerInfo.lng);
+                });
+            });
+
+        });
+        this.infoWindows.push(infoWindow);
+    }
+
+    closeAllInfoWindows() {
+        for (let window of this.infoWindows) {
+            window.close();
+        }
+    }
 }
